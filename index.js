17e7e9d73f22e4f7ac17667ec1fc898564ca85f6
DarkPark <darkpark@pisem.net>
2015-12-26 16:44:49 +0200

    replace tabs with spaces

--- index.js
+++ index.js
@@ -11,13 +11,13 @@
 /* eslint-disable */
 
 if ( !window.requestAnimationFrame ) {
-	// shim layer with setTimeout fallback
-	window.requestAnimationFrame =
-		window.mozRequestAnimationFrame ||
-		window.webkitRequestAnimationFrame ||
-		window.msRequestAnimationFrame ||
-		function ( callback ) {
-			window.setTimeout(callback, 1000 / 60);
-		};
+    // shim layer with setTimeout fallback
+    window.requestAnimationFrame =
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function ( callback ) {
+            window.setTimeout(callback, 1000 / 60);
+        };
 }
 
